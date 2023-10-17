5a0d479dea2c9a37ec997dbb358505c3d6c268d2
alexanderrud <rudnicenko97@gmail.com>
2023-10-17 23:44:23 +0300

    added new components. made some refactoring

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from './components/Header'
+import Container from "./components/Container";
 
 function App() {
     return (
@@ -7,6 +8,9 @@ function App() {
             <header className="App-header">
                 <Header></Header>
             </header>
+            <main className="App-main">
+                <Container></Container>
+            </main>
         </div>
     );
 }
---
+++ src/components/Container.js
@@ -0,0 +1,9 @@
+function Container() {
+    return (
+        <div className="Container">
+            <h1>Hello</h1>
+        </div>
+    )
+}
+
+export default Container;
